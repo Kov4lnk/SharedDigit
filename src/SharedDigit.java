67c0d9e155e23d8a22cbf69c0b14b9a1fc508d83
Kov4lnk <kov4lnk@gmail.com>
2021-01-25 19:21:17 +0200

    Initial commit

---
+++ src/SharedDigit.java
@@ -0,0 +1,12 @@
+public class SharedDigit {
+    public static boolean hasSharedDigit(int first, int second){
+        if (first < 10 || first > 99 || second < 10 || second > 99){
+            return false;
+        }
+
+        return  ((first % 10 == second % 10) || (first % 10 == second / 10) || (first / 10
+        == second % 10) || (first / 10 == second / 10));
+
+    }
+}
+
